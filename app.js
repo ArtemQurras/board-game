dca7ea727512413df044b06d7923ba43d548d64c
Artem <115409043+ArtemQurras@users.noreply.github.com>
2023-03-19 06:29:17 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const board = document.querySelector('#board')
+const colors = [
+    '#814444',
+    '#30833a91',
+    '#8e1b7391',
+    '#581b9d91',
+    '#17052c91',
+    '#a5161691',
+    '#a4a21891']
+
+const SQUARES_NUMBER = 500
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => 
+    setColor(square))
+
+    square.addEventListener('mouseleave', () => 
+    removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
